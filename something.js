06240937c0e1768a143841a7a340e09f9873e1ca
mgokulgoku <133188868+mgokulgoku@users.noreply.github.com>
2025-08-06 22:28:23 +0530

    feat : added some code

--- something.js
+++ something.js
@@ -7,7 +7,25 @@ const btn = document.getElementById('btn')
             { name: 'Steve', email: 'steve@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=2' },
             { name: 'Elon', email: 'elon@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=3' }];
 
-            
+            const btn = document.getElementById('btn')
+        const CardContent = document.getElementById('CardContent')
+
+        const generateCard = () => {
+
+            const userDetails = [{ name: 'Gokul', email: 'gokul@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=1' },
+            { name: 'Steve', email: 'steve@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=2' },
+            { name: 'Elon', email: 'elon@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=3' }];
+
+            const randNum = userDetails[Math.floor(Math.random() * userDetails.length)];
+            CardContent.innerHTML = `<div class="userCard">
+                                             <img src= ${randNum.photo}></img>
+                                             <h3>${randNum.name}</h3>
+                                             <p>${randNum.email}</p>
+                                             <p>${randNum.city}</p>
+                                             </div>`;
+        }
+
+        btn.addEventListener('click', generateCard)   
         }
 
         btn.addEventListener('click', generateCard)   
