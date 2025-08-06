e4c6d9c678d23cf6dbe306b8fc7cc7a17c76b7c6
mgokulgoku <133188868+mgokulgoku@users.noreply.github.com>
2025-08-06 22:27:34 +0530

    fix : deleted some code

--- something.js
+++ something.js
@@ -7,13 +7,7 @@ const btn = document.getElementById('btn')
             { name: 'Steve', email: 'steve@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=2' },
             { name: 'Elon', email: 'elon@example.com', city: 'karaikudi', photo: 'https://i.pravatar.cc/80?img=3' }];
 
-            const randNum = userDetails[Math.floor(Math.random() * userDetails.length)];
-            CardContent.innerHTML = `<div class="userCard">
-                                             <img src= ${randNum.photo}></img>
-                                             <h3>${randNum.name}</h3>
-                                             <p>${randNum.email}</p>
-                                             <p>${randNum.city}</p>
-                                             </div>`;
+            
         }
 
         btn.addEventListener('click', generateCard)   
